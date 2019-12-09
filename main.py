6b702ebc2143e1f7dbe6855da463f76382da7ace
submarrin <18marinam@gmail.com>
2019-12-09 18:27:11 +0500

    works for the multiplication of weights as a path weight. Done. Need to write it in file_out

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def read_graph():
     for i in range(n):
         graph_matrix[i] = [None] * n
     for i in range(n):
-        line = file_in.readline().split() #array
-        # print(pairs)
+        line = file_in.readline().split()  # array
         for j in range(n):
             graph_matrix[i][j] = int(line[j])
     start = int(file_in.readline())
@@ -15,31 +14,60 @@ def read_graph():
     return graph_matrix, int(start) - 1, int(finish) - 1
 
 
+def get_path_from_array_of_previous(array_of_previous, start, finish):
+    path = []
+    temporary_vertex = finish
+    while temporary_vertex != -1:
+        path.append(temporary_vertex)
+        temporary_vertex = array_of_previous[temporary_vertex]
+    return path
+
+
+def find_vertex_with_min_distance_from_set_not_visited(set_not_visited, distance_array, min_distance):
+    min_vertex = None
+    for vertex in set_not_visited:
+        if distance_array[vertex] < min_distance:
+            min_distance = distance_array[vertex]
+    for vertex in set_not_visited:
+        if distance_array[vertex] == min_distance:
+            min_vertex = vertex
+    return min_vertex
+
+
 def find_shortest_path(matr, start, finish):
     n = len(matr)
-    previous = [None]*n
+    previous = [None] * n
     set_not_visited = [item for item in range(n)]
-    final_distance = [32767]*n
-    final_distance[start] = 0
+    distance = [32767] * n
+    distance[start] = 0
     set_not_visited.remove(start)
+    previous[start] = -1
     print("set not visited: ", set_not_visited)
     for vertex in set_not_visited:
-        final_distance[vertex] = matr[start][vertex]
+        distance[vertex] = matr[start][vertex]
         previous[vertex] = start
-    for i in range(n-1):
-        max_distance_from_not_visited = -1
+    for i in range(n - 1):
+        print("i = ", i + 1)
+        min_distance_from_not_visited = 32767
+        vertex_with_min_distance = find_vertex_with_min_distance_from_set_not_visited(set_not_visited, distance,
+                                                                                      min_distance_from_not_visited)
+        set_not_visited.remove(vertex_with_min_distance)
+        print("set not visited = ", set_not_visited)
+        # finding min distance
         for vertex in set_not_visited:
-            if final_distance[vertex] > max_distance_from_not_visited:
-                max_distance_from_not_visited = final_distance[vertex]
-        vertex_with_max_distance = final_distance.index(max_distance_from_not_visited)
-        set_not_visited.remove(vertex_with_max_distance)
-        for vertex in set_not_visited:
-            if final_distance[vertex_with_max_distance] + matr[vertex_with_max_distance][vertex] > final_distance[vertex]:
-                final_distance[vertex] = final_distance[vertex_with_max_distance] + matr[vertex_with_max_distance][vertex]
-                previous[vertex] = vertex_with_max_distance
-        print(previous)
-    print(final_distance)
-    return final_distance[finish]
+            if distance[vertex_with_min_distance] * matr[vertex_with_min_distance][vertex] < distance[vertex] and \
+                    matr[vertex_with_min_distance][vertex] != 32767:
+                distance[vertex] = distance[vertex_with_min_distance] * matr[vertex_with_min_distance][vertex]
+                previous[vertex] = vertex_with_min_distance
+    print("array of final distance", distance)
+    reversed_path = get_path_from_array_of_previous(previous, start, finish)
+    path = []
+    for vertex in reversed_path:
+        vertex += 1
+        path.append(vertex)
+    path.reverse()
+    print("path = ", path)
+    return distance[finish]
 
 
 if __name__ == "__main__":
@@ -47,4 +75,3 @@ if __name__ == "__main__":
     print(find_shortest_path(matr, start, finish))
     # don't forget to add 1 to all the vertices in final result
     pass
-
